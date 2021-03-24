2d3feea611985c871c943d413f138a8828786286
Rafael Valdez <manrvaldez.92@gmail.com>
2021-03-24 07:31:35 -0500

    PizzaContext Created

---
+++ Relevant pizza/Data/PizzaContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Relevant_pizza.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Relevant_pizza.Data
+{
+    public class PizzaContext : DbContext
+    {
+        public PizzaContext(DbContextOptions<PizzaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
