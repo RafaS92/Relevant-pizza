cdeb3ef23889f4cd3784dea40af2d2c35837a06f
Rafael Valdez <manrvaldez.92@gmail.com>
2021-03-23 21:40:08 -0500

    Add project files.

---
+++ Relevant pizza/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Relevant_pizza.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
